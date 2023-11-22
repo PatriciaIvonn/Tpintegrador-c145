7cae79b1109dc68f2e17c33cc3820889cd796949
SilJavJor <silvio.jorda@gmail.com>
2023-11-22 18:19:50 -0300

    Commit proyrecto con maven, springboot y mysql

---
+++ TpIntegrado/src/main/java/com/ap/tpintegrado/TpIntegradoApplication.java
@@ -0,0 +1,13 @@
+package com.ap.tpintegrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIntegradoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpIntegradoApplication.class, args);
+    }
+
+}
---
+++ TpIntegrado/src/test/java/com/ap/tpintegrado/TpIntegradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.tpintegrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIntegradoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
